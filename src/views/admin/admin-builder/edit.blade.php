063ad4915fa3e5649e93bbeda28536a270a481b2
Alexandr Fanamurov <fanamura@gmail.com>
2017-08-01 15:49:04 +1000

    Fix switcher

--- src/views/admin/admin-builder/edit.blade.php
+++ src/views/admin/admin-builder/edit.blade.php
@@ -3,36 +3,10 @@
 
 @section('content')
     <div class="container-head uk-margin-bottom">
-        <div class="add-panel uk-margin-bottom uk-text-right">
-            <a class="uk-button" href="#modal-help" data-uk-modal="{target:'#modal-help'}"><i class="uk-icon-question"></i></a>
-            @if(isset($data->get_category))
-                <a class="uk-button uk-button-primary" href="/admin/{{ $app->name }}/create?category={{ $data->get_category->first()->id }}">Добавить другой материал</a>
-            @else
-                <a class="uk-button uk-button-primary" href="/admin/{{ $app->name }}/create">Добавить другой материал</a>
-            @endif
-        </div>
-        <div id="modal-help" class="uk-modal">
-            <div class="uk-modal-dialog">
-                <a class="uk-modal-close uk-close"></a>
-                <p>{{ $app->description }}</p>
-            </div>
-        </div>
-        <div class="uk-clearfix"></div>
-        {!! Breadcrumbs::render('admin.'. $app->name .'.edit', $data) !!}
-        @if(isset($data->get_category) && count($data->get_category) > 1)
-            <div>В разделах:</div>
-            @foreach($data->get_category as $category)
-                <ul>
-                    <li>{{ $category->title }}: <a href="/{{ $app->name }}/{{ $category->url }}/{{ $data->url }}">/{{ $app->name }}/{{ $category->url }}/{{ $data->url }}</a></li>
-                </ul>
-            @endforeach
-        @else
-            <div class="uk-clearfix"></div>
-            <a class="link-blank" href="{{ $data->full_url }}">{{ $data->full_url }}</a>
-        @endif
+        {!! Breadcrumbs::render('admin.'. $app->name .'.create') !!}
     </div>
 
-    <ul class="uk-tab uk-margin-large-top" data-uk-tab="{connect:'#tab-content, #tab-content-plugins'}">
+    <ul class="uk-tab" data-uk-tab="{connect:'#tab-content, #tab-content-plugins'}">
         @foreach($app->tabs as $tabs_key => $tabs_value)
             <li class="@if($loop->first) uk-active @endif">
                 <a href="">{{ $tabs_value }}</a>
@@ -49,7 +23,11 @@
     </ul>
 
     <div class="ibox-content">
-        <form id="edit-form-build" class="validate uk-form uk-form-stacked" action="/admin/{{ $app->name }}/{{ $data->id }}" method="POST" novalidate="novalidate">
+        <form class="validate uk-form uk-form-stacked" action="/admin/{{ $app->name }}" method="POST" novalidate="novalidate">
+            <input name="_method" type="hidden" value="POST">
+            <input name="type_connect" type="hidden" value="{{ $app->name }}">
+            <input name="id_connect" type="hidden" value="{{ DB::table($app->table)->max('id') +1 }}">
+
             <ul id="tab-content" class="uk-switcher uk-margin">
                 @foreach($app->tabs_data as $tabs_key => $tabs_value)
                     <li id="tab{{ $tabs_key }}">
@@ -60,11 +38,8 @@
             </ul>
 
             <div class="uk-align-right">
-                <input name="_method" type="hidden" value="PUT">
-                <input name="type_connect" type="hidden" value="{{ $app->name }}">
-                <input name="id_connect" type="hidden" value="{{ $data->id }}">
                 {{ csrf_field() }}
-                <button type="submit" class="uk-button uk-button-primary uk-button-large uk-hidden" form="edit-form-build">Сохранить</button>
+                <button type="submit" class="uk-button uk-button-primary uk-button-large">Сохранить</button>
             </div>
             <div class="uk-clearfix"></div>
         </form>
@@ -83,28 +58,5 @@
                 @endforeach
             </ul>
         @endif
-    </div>
-
-    <div class="uk-grid uk-margin-top buttons-save">
-        <div class="uk-width-1-2">
-            <form class="uk-form" action="/admin/{{ $app->name }}/{{ $data->id }}" method="post" id="test">
-                <input name="_method" type="hidden" value="DELETE">
-                <input name="id_connect" type="hidden" value="{{ $data->id }}">
-                <input name="type_connect" type="hidden" value="{{ $app->name }}">
-                <input name="place" type="hidden" value="material">
-                {{ csrf_field() }}
-                <button type="submit" class="uk-button uk-button-danger uk-button-large please_conform">Удалить материал</button>
-            </form>
-        </div>
-        <div class="uk-width-1-2 uk-text-right">
-            <button type="submit" class="uk-button uk-button-primary uk-button-large" form="edit-form-build">Сохранить</button>
-        </div>
-    </div>
-
-    <div id="process-modal" class="uk-modal">
-        <div class="uk-modal-dialog">
-            <a class="uk-modal-close uk-close"></a>
-            <p class="text-process-modal"></p>
-        </div>
     </div>
 @endsection
