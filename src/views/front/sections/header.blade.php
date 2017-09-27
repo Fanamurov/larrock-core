d04664f83626264d1059f59e08279d7041436630
Alexandr Fanamurov <fanamura@gmail.com>
2017-09-27 18:11:14 +1000

    add alt

--- src/views/front/sections/header.blade.php
+++ src/views/front/sections/header.blade.php
@@ -2,7 +2,7 @@
     <div class="uk-container uk-container-center">
         <div class="uk-grid">
             <div class="uk-width-1-1 uk-width-small-1-2 uk-width-medium-2-3 uk-width-large-1-3">
-                <a href="/"><img class="logo" src="/_assets/_front/_images/logo.png" srcset="/_assets/_front/_images/logo@2x.png 2x"></a>
+                <a href="/"><img class="logo" src="/_assets/_front/_images/logo.png" srcset="/_assets/_front/_images/logo@2x.png 2x" alt="{{ env('SITE_NAME') }}"></a>
                 <address class="uk-hidden-small">
                     @if(isset($telefony_v_shapke))
                         @include('larrock::front.modules.html.text', ['data' => $telefony_v_shapke])
