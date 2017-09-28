110cb8f123ff01a05a852a25629de9e977ab6586
Alexandr Fanamurov <fanamura@gmail.com>
2017-09-28 10:41:33 +1000

    fix error text

--- src/views/front/errors.blade.php
+++ src/views/front/errors.blade.php
@@ -2,7 +2,7 @@
     @foreach($errors->all() as $error)
         <div class="uk-alert uk-alert-danger alert-dismissable">
             <a href="" class="uk-alert-close uk-close"></a>
-            {{ $error }}
+            {!! $error !!}
         </div>
     @endforeach
 @endif
@@ -11,7 +11,7 @@
         @php if($type === 'error'){ $type = 'danger';} @endphp
         <div class="uk-alert uk-alert-{{ $type }} alert-dismissable">
             <a href="" class="uk-alert-close uk-close"></a>
-            {{ $message }}
+            {!! $message !!}
         </div>
     @endforeach
 @endforeach
