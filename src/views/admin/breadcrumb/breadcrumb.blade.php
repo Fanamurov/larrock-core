43867168774a0f8644732d18120149a82d29c52e
Alexandr Fanamurov <fanamurov@yandex.ru>
2017-10-01 12:22:40 +1000

    Update breadcrumb.blade.php
    fix bug

--- src/views/admin/breadcrumb/breadcrumb.blade.php
+++ src/views/admin/breadcrumb/breadcrumb.blade.php
@@ -1,7 +1,7 @@
 @if ($breadcrumbs)
     <ol class="breadcrumb" data-count="{{ $count = count($breadcrumbs) }}">
         @foreach ($breadcrumbs as $breadcrumb)
-            @if ($breadcrumb->url && !$breadcrumb->last)
+            @if ($breadcrumb->url && !$loop->last)
                 <li><h2><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></h2></li>
             @else
                 <li class="active"><h1>{{ $breadcrumb->title }}</h1></li>
