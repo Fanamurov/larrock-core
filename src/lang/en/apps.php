b5caa7ca53e61a4c776cbe52d1c0d12a11223671
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-31 15:47:16 +1000

    add en lang file

---
+++ src/lang/en/apps.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	'update' => array(
+		'success' => 'Content :name successfully changed',
+		'error' => 'Content :name not changed',
+        'nothing' => 'Content :name not required changes'
+	),
+
+    'delete' => array(
+        'success' => 'Content :name успешно удален',
+        'error' => 'Content :name не удален'
+    ),
+
+    'create' => [
+        'success' => 'Content :name successfully created',
+        'success-temp' => 'Draft material added successfully',
+        'error' => 'Failed to add content :name',
+    ]
+
+];
