20f65003c325fbe96446a515aa3d79ca3c0db924
Alexandr Fanamurov <fanamurov@ya.ru>
2018-01-16 15:29:57 +1000

    update install command

--- src/Commands/LarrockInstallCommand.php
+++ src/Commands/LarrockInstallCommand.php
@@ -29,27 +29,31 @@ class LarrockInstallCommand extends Command
     {
         $this->line('=== Install LarrockCMS ===');
 
-        if ($this->confirm('Обновить .env? (larrock:updateEnv)')) {
+        if ($this->confirm('Шаг 1/7. Обновить .env? (larrock:updateEnv)')) {
             $this->call('larrock:updateEnv');
         }
 
-        if ($this->confirm('Сменить директорию "public" на "public_html"? (larrock:renamePublicDirectory)')) {
+        if ($this->confirm('Шаг 2/7. Сменить директорию "public" на "public_html"? (larrock:renamePublicDirectory)')) {
             $this->call('larrock:renamePublicDirectory');
         }
 
-        if ($this->confirm('Обновить конфиги зависимостей? (larrock:updateVendorConfig)')) {
+        if ($this->confirm('Шаг 3/7. Обновить конфиги зависимостей? (larrock:updateVendorConfig)')) {
             $this->call('larrock:updateVendorConfig');
         }
 
-        if ($this->confirm('Хотите установить пакеты не входящие в ядро LarrockCMS? (larrock:manager)')) {
+        if ($this->confirm('Шаг 4/7. Хотите установить пакеты не входящие в ядро LarrockCMS? (larrock:manager)')) {
             $this->call('larrock:manager');
         }
 
-        if ($this->confirm('Опубликовать ресурсы (vendor:publish)?')) {
+        if ($this->confirm('Шаг 5/7. Опубликовать ресурсы (vendor:publish)?')) {
             $this->call('vendor:publish');
         }
 
-        if ($this->confirm('Добавить пользователя администратора? (larrock:addAdmin)')) {
+        if ($this->confirm('Шаг 6/7. Опубликовать ресурсы (migrate)?')) {
+            $this->call('migrate');
+        }
+
+        if ($this->confirm('Шаг 7/7. Добавить пользователя администратора? (larrock:addAdmin)')) {
             $this->call('larrock:addAdmin');
         }
 
