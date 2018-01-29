88099f497d5faa3066f757f42bed8c2c9ec557a5
Alexandr Fanamurov <fanamurov@ya.ru>
2018-01-29 18:08:22 +1000

    update getCostLink(), add getCostValuesAttribute()

--- src/Traits/GetLink.php
+++ src/Traits/GetLink.php
@@ -51,13 +51,19 @@ trait GetLink{
     /**
      * Получение связи модификации цены
      * @param $childModel
+     * @param $modelChildWhereKey
+     * @param $modelChildWhereValue
      * @return null
      */
-    public function getCostLink($childModel)
+    public function getCostLink($childModel, $modelChildWhereKey = NULL, $modelChildWhereValue = NULL)
     {
         $cache_key = sha1('getCostLink'. $this->config->model . $childModel . $this->id);
-        return Cache::remember($cache_key, 1140, function () use ($childModel) {
-            if($getLink = $this->hasMany(Link::class, 'id_parent')->whereModelParent($this->config->model)->whereModelChild($childModel)->get()){
+        return Cache::remember($cache_key, 1140, function () use ($childModel, $modelChildWhereKey, $modelChildWhereValue) {
+            $query = $this->hasMany(Link::class, 'id_parent')->whereModelParent($this->config->model)->whereModelChild($childModel);
+            if($modelChildWhereKey && $modelChildWhereValue){
+                $query->where($modelChildWhereKey, '=', $modelChildWhereValue);
+            }
+            if($getLink = $query->get()){
                 foreach ($getLink as $key => $item){
                     $class = new $childModel();
                     if($param = $class->whereId($item->id_child)->first()){
@@ -66,6 +72,29 @@ trait GetLink{
                 }
                 return $getLink;
             }
+        });
+    }
+
+    /**
+     * Получение модификаций товаров
+     * Товар->cost_value
+     * @return mixed|null
+     */
+    public function getCostValuesAttribute()
+    {
+        $cache_key = sha1('cost_value'. $this->id);
+        return Cache::remember($cache_key, 1140, function () {
+            $values = NULL;
+            foreach ($this->config->rows as $row){
+                if(isset($row->costValue) && $row->costValue){
+                    if( !isset($values)){
+                        $values = $this->getCostLink($row->modelChild, $row->modelChildWhereKey, $row->modelChildWhereValue);
+                    }else{
+                        $values = $values->merge($this->getCostLink($row->modelChild, $row->modelChildWhereKey, $row->modelChildWhereValue));
+                    }
+                }
+            }
+            return $values;
         });
     }
 }
