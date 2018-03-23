43525a5f4e552c100c0041390eec698206cb4dcd
Alexandr Fanamurov <fanamurov@ya.ru>
2018-03-23 17:13:13 +1000

    add test

--- tests/Plugins/ComponentPluginTest.php
+++ tests/Plugins/ComponentPluginTest.php
@@ -65,6 +65,8 @@ class ComponentPluginTest extends TestCase
     public function testAttach()
     {
         $component = new BlocksComponent();
+
+        //Случай когда нужно seo создать
         $request = Request::create('/', 'POST', [
             'seo_title' => 'seo_title_test',
             'seo_description' => 'seo_description_test',
@@ -74,10 +76,47 @@ class ComponentPluginTest extends TestCase
         ]);
         $data = Blocks::whereId(1)->first();
         $event = new ComponentItemStored($component, $data, $request);
+        $this->componentPlugin->attach($event);
+
+        //Случай когда seo нужно удалить
+        \DB::connection()->table('seo')->insert([
+            'seo_title' => 'test',
+            'seo_description' => 'test',
+            'seo_keywords' => 'test',
+            'seo_id_connect' => 1,
+            'seo_url_connect' => 'test',
+            'seo_type_connect' => 'blocks',
+        ]);
+
+        $request = Request::create('/', 'POST', [
+            'seo_title' => 'seo_title_test',
+            'seo_description' => 'seo_description_test',
+            'seo_seo_keywords' => 'seo_keywords_test',
+            'type_connect' => 'blocks',
+            'id_connect' => 1
+        ]);
+        $event = new ComponentItemStored($component, $data, $request);
+        $this->componentPlugin->attach($event);
+
+        //Случай когда с сео ничего не должно произойти
+        $request = Request::create('/', 'POST');
+        $event = new ComponentItemStored($component, $data, $request);
         $test = $this->componentPlugin->attach($event);
+
         $this->assertNull($test);
 
         /*$testData = Blocks::whereTypeConnect('blocks')->whereIdConnect(1)->first();
         dd($testData);*/
+    }
+
+    public function testDetach()
+    {
+        $component = new BlocksComponent();
+        $request = Request::create('/', 'POST');
+        $data = Blocks::whereId(1)->first();
+        $event = new ComponentItemStored($component, $data, $request);
+        $test = $this->componentPlugin->attach($event);
+
+        $this->assertNull($test);
     }
 }
