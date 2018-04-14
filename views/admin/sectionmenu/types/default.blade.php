80d24984572077cc101dc9f41127e6dfb5e833b5
Alexandr Fanamurov <fanamurov@yandex.ru>
2018-04-14 19:32:49 +1000

    hot fix @package var in template

--- views/admin/sectionmenu/types/default.blade.php
+++ views/admin/sectionmenu/types/default.blade.php
@@ -1,3 +1,3 @@
-<li @if(str_contains(\URL::current(), $url)) class="uk-active" @endif title="{{ $package->description }}">
-    <a href="{{ $url }}">{{ $package->title }} @if(isset($count))<span class="count">[{{ $count }}]</span>@endif</a>
+<li @if(str_contains(\URL::current(), $url)) class="uk-active" @endif title="{{ $app->description }}">
+    <a href="{{ $url }}">{{ $app->title }} @if(isset($count))<span class="count">[{{ $count }}]</span>@endif</a>
 </li>
